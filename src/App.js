23fd709ba09e26098bcb1c176dc8ad9283d8c547
Olek Myron <myrondell@gmail.com>
2021-08-24 16:08:09 +0300

    done 1st task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-// import logo from './logo.svg';
 import './App.css';
+import Profile from './components/Profile';
+import { name, tag, location, avatar, stats } from './components/Profile/user.json';
 
 function App() {
   return (
     <div className="App">
-      ...
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      {
+        <Profile
+          name={name}
+          tag={tag}
+          location={location}
+          avatar={avatar}
+          followers={stats.followers}
+          views={stats.views}
+          likes={stats.likes}
+        />
+      }
     </div>
   );
 }
