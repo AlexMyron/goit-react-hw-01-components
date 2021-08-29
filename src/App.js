17bd8951c37d7a83c279509b7906bca482a97077
Olek Myron <myrondell@gmail.com>
2021-08-29 19:59:31 +0300

    passed props with one single stat

--- src/App.js
+++ src/App.js
@@ -11,15 +11,7 @@ import transactions from './components/TransactionHistory/transactions.json';
 function App() {
   return (
     <div className="App">
-      <Profile
-        name={name}
-        tag={tag}
-        location={location}
-        avatar={avatar}
-        followers={stats.followers}
-        views={stats.views}
-        likes={stats.likes}
-      />
+      <Profile name={name} tag={tag} location={location} avatar={avatar} statistics={stats} />
 
       <Statistics title="Upload stats" stats={statisticalData} />
       <Statistics stats={statisticalData} />
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import s from './Profile.module.css';
 
-function Profile({ name, tag, location, avatar, followers, views, likes }) {
+function Profile({ name, tag, location, avatar, statistics }) {
   return (
     <div className={s.profile}>
       <div className={s.description}>
@@ -14,15 +14,15 @@ function Profile({ name, tag, location, avatar, followers, views, likes }) {
       <ul className={s.stats}>
         <li>
           <span className={s.label}>Followers </span>
-          <span className={s.quantity}>{followers}</span>
+          <span className={s.quantity}>{statistics.followers}</span>
         </li>
         <li>
           <span className={s.label}>Views </span>
-          <span className={s.quantity}>{views}</span>
+          <span className={s.quantity}>{statistics.views}</span>
         </li>
         <li>
           <span className={s.label}>Likes </span>
-          <span className={s.quantity}>{likes}</span>
+          <span className={s.quantity}>{statistics.likes}</span>
         </li>
       </ul>
     </div>
@@ -34,9 +34,11 @@ Profile.propTypes = {
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
-  followers: PropTypes.number.isRequired,
-  views: PropTypes.number.isRequired,
-  likes: PropTypes.number.isRequired,
+  statistics: PropTypes.exact({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
 };
 
 export default Profile;
